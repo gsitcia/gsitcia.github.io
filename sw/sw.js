0e8313d6ede60b6cc035f54efdf0fbb6e0073c17
Walter Mitty <gsitcia@gmail.com>
2018-12-18 14:37:14 -0600

    Update sw.js

--- sw/sw.js
+++ sw/sw.js
@@ -9,7 +9,7 @@ self.addEventListener('push', function(event) {
 
   event.waitUntil(self.registration.showNotification(title, options));
 });
-
+/*
 self.addEventListener('install', function(e) {
   e.waitUntil(self.registration.showNotification('Notification is set up!'));
 });
